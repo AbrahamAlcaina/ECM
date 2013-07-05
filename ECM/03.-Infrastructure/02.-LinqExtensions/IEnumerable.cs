8b8e007e457e640cb91aa0469d3357b098062075
Abraham Alcaina <abraham.alcaina@gmail.com>
2013-07-05 13:24:42 +0200

    Remove ServiceStack trash. Nuget updated

---
+++ ECM/03.-Infrastructure/02.-LinqExtensions/IEnumerable.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IEnumerable.cs" company="Abraham Alcaina">
+//   Abraham Alcaina
+// </copyright>
+// <summary>
+//   The enumerable extensions.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace System
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    ///     The enumerable extensions.
+    /// </summary>
+    internal static class EnumerableExtensions
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The for each.
+        /// </summary>
+        /// <param name="enumeration">
+        /// The enumeration.
+        /// </param>
+        /// <param name="action">
+        /// The action.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+        {
+            foreach (T item in enumeration)
+            {
+                action(item);
+            }
+        }
+
+        #endregion
+    }
+}
