81eccbc96e1457ad7902ff0abc88facf0f2d8a50
Abraham Alcaina <abraham.alcaina@gmail.com>
2013-07-06 10:34:48 +0200

    Add host test

---
+++ ECM.Test/03.-Infrastructure/AppHostTest.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AppHostTest.cs" company="Abraham Alcaina">
+//   Abraham Alcaina
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ECM.Test._03._Infrastructure
+{
+    using ECM.Domain.Entities;
+    using ECM.Infrastructure.Repositories;
+    using ECM.Infrastructure.ServiceStack;
+
+    using MongoRepository;
+
+    using Xunit;
+
+    using global::ServiceStack.WebHost.Endpoints;
+
+    /// <summary>
+    /// The app host test.
+    /// </summary>
+    public class AppHostTest
+    {
+        /// <summary>
+        /// Test register types in container.
+        /// </summary>
+        [Fact]
+        public void TypesRegisterInContainer()
+        {
+            // arrange
+            var sut = new AppHost();
+
+            // act
+            var result = sut.Container.Resolve<IRepository<File>>();
+
+            // assert
+            Assert.NotNull(result);
+            Assert.IsType<FileRepository>(result);
+        }
+    }
+}
